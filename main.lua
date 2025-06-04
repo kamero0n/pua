d2669f7f2cedd0d43114fc2c1490eb5548568a79
Cameron Gomez <71044334+kamero0n@users.noreply.github.com>
2025-06-03 19:47:25 -0700

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,18 @@
+--[[
+    description: making a moving rectangle
+]]
+
+function love.load()
+    x = 100
+end
+
+
+function love.update(dt)
+    x = x + 100 * dt
+end
+
+
+function love.draw()
+
+    love.graphics.rectangle("line", x, 50, 200, 150);
+end
