9e3323b8ca14e9dc5b0483dfcf1ea9b48f1d4a44
kamero0n <cagomez@usc.edu>
2025-06-03 22:09:17 -0700

    ball move up and down

--- main.lua
+++ main.lua
@@ -3,13 +3,13 @@
 ]]
 
 -- get window width & height
+-- i think it's like 800 x 600
 WINDOWWIDTH, WINDOWHEIGHT = love.graphics.getDimensions()
 
 
 function love.load()
     -- wall stuff
     wallHeight = 10
-   -- bottomWallY = 590
 
     -- paddle stuff
     paddleX = 20
@@ -17,20 +17,37 @@ function love.load()
     paddleHeight = 70
     paddleSpeed = 300
 
+    -- ball stuff
+    ballX = 300
+    ballY = 400
+    ballDir = 1
+    ballSpeed = 300
+
 end
 
 
 function love.update(dt)
+    -- paddle movements
     if love.keyboard.isDown("down") then
         paddleY = paddleY + paddleSpeed * dt
     elseif love.keyboard.isDown("up") then
         paddleY = paddleY - paddleSpeed * dt
     end
 
+    -- paddle constraints/limits
     if paddleY <= wallHeight then
         paddleY = wallHeight
     elseif paddleY >= (WINDOWHEIGHT - wallHeight) - paddleHeight then
         paddleY = (WINDOWHEIGHT - wallHeight) - paddleHeight
+    end
+
+    -- ball movements
+    ballY = ballY + ballSpeed * ballDir * dt
+
+    if ballY >= (WINDOWHEIGHT - wallHeight) - 10 then
+        ballDir = -1
+    elseif ballY <= wallHeight then
+        ballDir = 1
     end
 end
 
@@ -40,9 +57,12 @@ function love.draw()
     love.graphics.rectangle("fill", paddleX, paddleY, 10, paddleHeight);
 
     -- top wall needs to be drawn
-    love.graphics.rectangle("line", 0, 0, WINDOWWIDTH, wallHeight);
+    love.graphics.rectangle("fill", 0, 0, WINDOWWIDTH, wallHeight);
 
     -- bottom wall needs to be drawn
-    love.graphics.rectangle("line", 0, WINDOWHEIGHT - wallHeight, WINDOWWIDTH, wallHeight);
+    love.graphics.rectangle("fill", 0, WINDOWHEIGHT - wallHeight, WINDOWWIDTH, wallHeight);
+
+    -- ball time
+    love.graphics.rectangle("fill", ballX, ballY, 10, 10);
 
 end
