4565ae6fd4b0884be7632262ad804edd8b49c317
kamero0n <cagomez@usc.edu>
2025-06-03 21:42:11 -0700

    added some nice walls, and can collide w/ them

--- main.lua
+++ main.lua
@@ -6,10 +6,12 @@ function love.load()
     -- wall stuff
     wallWidth = 800
     wallHeight = 10
+    bottomWallY = 590
 
     -- paddle stuff
     paddleX = 10
     paddleY = 5
+    paddleHeight = 70
     paddleSpeed = 300
 
 
@@ -22,17 +24,23 @@ function love.update(dt)
     elseif love.keyboard.isDown("up") then
         paddleY = paddleY - paddleSpeed * dt
     end
+
+    if paddleY <= wallHeight then
+        paddleY = wallHeight
+    elseif paddleY >= bottomWallY - paddleHeight then
+        paddleY = bottomWallY - paddleHeight
+    end
 end
 
 
 function love.draw()
     -- pong paddle
-    love.graphics.rectangle("fill", paddleX, paddleY, 10, 70);
+    love.graphics.rectangle("fill", paddleX, paddleY, 10, paddleHeight);
 
     -- top wall needs to be drawn
-    love.graphics.rectangle("line", 0, 0, wallWidth, wallHeight);
+    love.graphics.rectangle("fill", 0, 0, wallWidth, wallHeight);
 
     -- bottom wall needs to be drawn
-    love.graphics.rectangle("line", 0, 590, wallWidth, wallHeight);
+    love.graphics.rectangle("fill", 0, bottomWallY, wallWidth, wallHeight);
 
 end
