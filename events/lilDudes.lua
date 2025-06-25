93334218b61ac4fdbf54aa8b0e0af5749eac59be
kamero0n <cagomez@usc.edu>
2025-06-25 02:00:42 -0700

    the lil dude walks back and forth now

--- events/lilDudes.lua
+++ events/lilDudes.lua
@@ -10,7 +10,10 @@ local lilGuy = {
     time = 0,
     walkSpeed = 4, -- frequency
     legSwing = 2, -- X amplitude
-    stepHeight = 2 -- Y amplitude
+    stepHeight = 2, -- Y amplitude
+    bobHeight = 2,
+    direction = 1, -- going right
+    animSpeed = 40 -- pixels/sec
 }
 
 
@@ -19,7 +22,8 @@ function LilDudes.drawDude()
     love.graphics.setColor(1, 1, 1, 1)
 
     -- draw a small rectangle (this is the body)
-    love.graphics.rectangle("fill", lilGuy.x, lilGuy.y, lilGuy.width, lilGuy.height)
+    local bodyBob = lilGuy.bobHeight * math.cos(lilGuy.time * lilGuy.walkSpeed * 2)
+    love.graphics.rectangle("fill", lilGuy.x, lilGuy.y + bodyBob, lilGuy.width, lilGuy.height)
 
     -- draw legs? left leg!
     local leftLegXOffset = lilGuy.legSwing * math.sin(lilGuy.time * lilGuy.walkSpeed)
@@ -34,11 +38,22 @@ function LilDudes.drawDude()
     -- set the canvas color to black
     love.graphics.setColor(0, 0, 0, 1)
 
-    -- draw eyes
-    love.graphics.rectangle("fill", lilGuy.x + lilGuy.width - 5, lilGuy.y + 10, 3, 5)
-    love.graphics.rectangle("fill", lilGuy.x + lilGuy.width - 10    , lilGuy.y + 10, 3, 5)
+    -- draw eyes (add body bob here so the eyes move w/ the head)
+    if lilGuy.direction == 1 then
+        love.graphics.rectangle("fill", lilGuy.x + lilGuy.width - 5, lilGuy.y + 10 + bodyBob, 3, 5)
+        love.graphics.rectangle("fill", lilGuy.x + lilGuy.width - 10, lilGuy.y + 10 + bodyBob, 3, 5)
+    else
+        love.graphics.rectangle("fill", lilGuy.x + 2, lilGuy.y + 10 + bodyBob, 3, 5)
+        love.graphics.rectangle("fill", lilGuy.x + 8, lilGuy.y + 10 + bodyBob, 3, 5)
+    end
 end
 
 function LilDudes.updateTime(time)
     lilGuy.time = lilGuy.time + time
+
+    lilGuy.x = lilGuy.x + (lilGuy.animSpeed * time) * lilGuy.direction
+
+    if lilGuy.x > WINDOWWIDTH - 30 - lilGuy.width then
+        lilGuy.direction = lilGuy.direction * (-1)
+    end
 end
